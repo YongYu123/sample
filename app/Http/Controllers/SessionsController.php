99882d77ebe5af825bcc5a81a6edb3b4f52cd497
YongYu123 <zdyy1989@gmail.com>
2018-01-22 03:38:40 +0000

    Finish user edit, update, index, and destroy actions

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -10,6 +10,12 @@ use Illuminate\Support\Facades\Hash;
 
 class SessionsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('guest', [
+            'only'=>['create']
+        ]);
+    }
     public function create()
     {
         return view('sessions.create');
@@ -20,9 +26,9 @@ class SessionsController extends Controller
             'email' => 'required|email|max:255',
             'password' => 'required'
         ]);
-        if (Auth::attempt($credentials,$request->has('remember'))) {
+        if (Auth::attempt($credentials, $request->has('remember'))) {
             session()->flash('success', '欢迎回来！');
-            return redirect()->route('users.show', [Auth::user()]);
+            return redirect()->intended(route('users.show', [Auth::user()]));
         } else {
             session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
             return redirect()->back();
@@ -31,9 +37,7 @@ class SessionsController extends Controller
     public function destroy()
     {
         Auth::logout();
-        session()->flash('success','您已成功退出！');
+        session()->flash('success', '您已成功退出！');
         return redirect('login');
     }
-
-
 }
--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,14 @@
 |
 */
 
-Route::get('/','StaticPagesController@home')->name('home');
-Route::get('/help','StaticPagesController@help')->name('help');
-Route::get('/about','StaticPagesController@about')->name('about');
-Route::get('/signup','UsersController@create')->name('signup');
-Route::resource('users','UsersController');
-Route::get('login','SessionsController@create')->name('login');
-Route::post('login','SessionsController@store')->name('login');
+Route::get('/', 'StaticPagesController@home')->name('home');
+Route::get('/help', 'StaticPagesController@help')->name('help');
+Route::get('/about', 'StaticPagesController@about')->name('about');
+Route::get('/signup', 'UsersController@create')->name('signup');
+Route::resource('users', 'UsersController');
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
+Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+Route::get('/index', 'UsersController@index')->name('users.index');
+Route::delete('/users/{user}/destroy', 'UsersController@destroy')->name('users.destroy');
